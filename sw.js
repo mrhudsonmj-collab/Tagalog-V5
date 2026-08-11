2af0f8a74c4bb92536063c26e8524222f2326442
mrhudsonmj-collab <mrhudsonmj@gmail.com>
2026-08-11 21:52:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tagalog-trainer-v53-elevenlabs';
+const CACHE='tagalog-trainer-v54-elevenlabs';
 const CORE=['./','./index.html','./app.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./phrases.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
